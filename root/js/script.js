f1feda4257789acde5a3b7be7fcad0552d40e60f
steisyGB <142046596+steisyGB@users.noreply.github.com>
2024-08-24 14:47:24 -0600

    Add files via upload

---
+++ root/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+    const gridItems = document.querySelectorAll('.grid-item');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('open');
+        navLinks.classList.toggle('active');
+    });
+
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', function() {
+            gridItems.forEach(item => item.classList.remove('active'));
+
+            const targetSection = document.querySelector(this.getAttribute('href'));
+            targetSection.classList.add('active');
+
+            // Quitar la clase 'active' después de 3 segundos
+            setTimeout(() => {
+                targetSection.classList.remove('active');
+            }, 1000);
+        });
+    });
+});
